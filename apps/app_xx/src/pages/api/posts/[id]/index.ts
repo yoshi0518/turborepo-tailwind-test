7dcf51fba128f6e94954b0867a9235a34aabea06
yoshi0518 <yoshihumi.n.0518@gmail.com>
2024-01-31 23:00:50 +0900

    API Routesへバリデーション機能を追加

--- apps/app_xx/src/pages/api/posts/[id]/index.ts
+++ apps/app_xx/src/pages/api/posts/[id]/index.ts
@@ -1,14 +1,34 @@
+import { NextApiHandler } from 'next';
+import { z } from 'zod';
+
 import type { PostType } from '@/features/posts/types';
+
+import { withZod } from '@/utils/zod';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<PostType | { message: string }>) => {
-  const { id } = req.query;
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+const handleGet = withZod(
+  z.object({
+    query: z.object({ id: z.string() }),
+  }),
+  async (req, res) => {
+    const id = req.query.id;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
 
-  if (response.status !== 200) res.status(500).json({ message: response.statusText });
+    if (response.status !== 200) res.status(500).json({ message: response.statusText });
 
-  res.status(200).json(await response.json());
+    res.status(200).json(await response.json());
+  },
+);
+
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<PostType | { message: string }>) => {
+  switch (req.method) {
+    case 'GET':
+      return handleGet(req, res);
+    default:
+      res.status(405).json({ message: 'Method Not Allowed' });
+      return;
+  }
 };
 
 export default handler;
